c26650b54edd68ba8a3b1b5d4941c4ebb1666883
kolesnikovz <kolesnikov1987ua@gmail.com>
2018-01-24 21:14:17 +0200

    fibonachi 3

--- fibonachi.php
+++ fibonachi.php
@@ -6,15 +6,25 @@
  * Time: 19:27
 */
 function fibonachi($n){
-    /**static $number = 0;
+    static $number = 0;
     $number ++;
     var_dump($number);
-     */
+
     if($n < 2){
         return $n;
     }else{
         return fibonachi($n - 1) + fibonachi($n - 2);
     }
 }
-echo fibonachi(32);
-
+echo fibonachi(29);
+/**
+$fibonachi = 5;
+$fibonachi = $n;
+for ($n = 0; ; $n++){
+    if($n < 2){
+        return $n;
+    }else{
+        return($n - 1) + ($n - 2);
+    }
+    echo $n;
+}*/
