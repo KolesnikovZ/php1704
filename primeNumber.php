1ac2a009e70e9a56ca463cd6884cc69763a210db
kolesnikovz <kolesnikov1987ua@gmail.com>
2018-01-25 03:39:09 +0200

    primeNumber 1

---
+++ primeNumber.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhenia
+ * Date: 24.01.2018
+ * Time: 21:18
+ */
+function primeNumber($n){
+    static $number = 0;
+    $number ++;
+    var_dump($number);
+    if(gmp_prob_prime($n) != 2){
+        echo "Fals";
+    }else{
+        echo $n;
+    }
+}
+echo primeNumber(19);
