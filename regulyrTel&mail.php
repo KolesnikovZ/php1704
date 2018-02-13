a61b98fd4efc84710a73559037afddc3a6b801ef
kolesnikovz <kolesnikov1987ua@gmail.com>
2018-02-13 17:57:18 +0200

    regulyr2

--- regulyrTel&mail.php
+++ regulyrTel&mail.php
@@ -7,8 +7,15 @@
  */
 $tel = '+38 093 9488814';
 $pattern = '#^((\+38|38)[\- ]?)?\(?\d{3}\)?[\- ]?[\d\- ]{7,9}$#';
-$count = preg_replace($pattern, $tel);
+$count = preg_match($pattern, $tel);
 var_dump($tel);
+var_dump($pattern);
+var_dump($count);
+
+$mail = 'info@company.com.ua';
+$pattern = '#^(([0-9A-Za-z]{1,}[\-\_\.0-9A-Za-z]{1,}[0-9A-Za-z])@(0-9A-Za-z\-]{1,}\.){1,2}[A-Za-z]{2,})$#';
+$count = preg_match($pattern, $mail);
+var_dump($mail);
 var_dump($pattern);
 var_dump($count);
 
